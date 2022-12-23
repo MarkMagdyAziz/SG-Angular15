fb7dbbb7e5e61da8d1332d5026bd7e99f5e7d8d0
Mark Magdy <mark.aziz.magdy@gmail.com>
2022-12-24 01:52:30 +0200

    handle filter by lang

--- src/app/landing/programs/programs.component.ts
+++ src/app/landing/programs/programs.component.ts
@@ -1,7 +1,6 @@
 import { IProgram } from './../../models/interfaces/iprogram';
 import { ProgramService } from './../../models/services/program.service';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-programs',
   templateUrl: './programs.component.html',
@@ -11,14 +10,26 @@ export class ProgramsComponent implements OnInit {
   programs: IProgram[] = []
   currentPage = 1;
   loader: boolean = true
-  constructor(private programService: ProgramService) {
 
+
+  // Filters
+  langs: any[] = [{ key: "English" }, { key: "French" }]
+  constructor(private programService: ProgramService) {
   }
   ngOnInit(): void {
     this.programService.getPrograms().subscribe(({
       next: (res) => this.programs = res,
-      error: (err) => console.log(err),
+      error: (err) => { console.log(err); this.loader = false },
       complete: () => this.loader = false
     }))
   }
+  filterLang(Lang: string) {
+    this.loader = true
+    this.programService.getPrograms(undefined, undefined, Lang).subscribe(({
+      next: (res) => this.programs = res,
+      error: (err) => { console.log(err); this.loader = false },
+      complete: () => this.loader = false
+    }))
+  }
+
 }
